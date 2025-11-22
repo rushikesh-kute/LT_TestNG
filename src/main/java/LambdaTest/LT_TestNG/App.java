eff89f90d54641ba7796d1cf52bb0a4b6ef5320e
rushi kute <rushikute955@gmail.com>
2025-11-22 12:52:04 +0530

    LT Testng
    LT test ng Project

---
+++ src/main/java/LambdaTest/LT_TestNG/App.java
@@ -0,0 +1,13 @@
+package LambdaTest.LT_TestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/LambdaTest/LT_TestNG/window_10_Chrome_TNg.java
@@ -0,0 +1,114 @@
+package LambdaTest.LT_TestNG;
+
+import java.net.URL;
+import java.util.HashMap;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.lambdatest.tunnel.Tunnel;
+
+public class window_10_Chrome_TNg{
+    WebDriver driver;
+    Tunnel t;
+    String URL = "https://www.lambdatest.com/selenium-playground/";
+
+    @Parameters({"browserVersion", "platform","username","access_key"})
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(String browserVersion,String platform,String username,String access_key) throws Exception { 
+    	
+    	ChromeOptions browserOptions = new ChromeOptions();
+    	browserOptions.addArguments("--ignore-certificate-errors");
+		browserOptions.setPlatformName(platform);
+		browserOptions.setBrowserVersion(browserVersion);
+		HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+		ltOptions.put("username",username);
+		ltOptions.put("accessKey", access_key);
+		ltOptions.put("build", "Build TestNG chrome - "+platform);
+		ltOptions.put("name", "TC Run TestNG Chrome -" + platform);
+		ltOptions.put("network",true);
+		ltOptions.put("console",true);
+		ltOptions.put("visual", true);
+		ltOptions.put("w3c", true);
+		browserOptions.setCapability("LT:Options", ltOptions);
+
+
+		//create tunnel instance
+		t = new Tunnel();
+		HashMap<String, String> options = new HashMap<String, String>();
+		options.put("user", username);
+		options.put("key", access_key);
+
+		//start tunnel
+		t.start(options);
+	
+		driver = new RemoteWebDriver(new URL("https://" + username + ":" + access_key + "@hub.lambdatest.com/wd/hub"), browserOptions);
+
+		System.out.println("Started session for Chrome" );
+		
+    }
+
+    @Test( timeOut = 20000)
+    public void Scenario1() throws InterruptedException {
+		System.out.println("Started senario 1");		
+		driver.get(URL);
+		Thread.sleep(3000);
+		WebDriverWait wait = new WebDriverWait(driver, 20000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Simple Form Demo")));
+
+        
+		SoftAssert softAssert = new SoftAssert();
+		String title = driver.getTitle();
+		softAssert.assertEquals(title, "LambdaTest", "Page Title is not as expected!");
+		softAssert.assertAll();
+
+    }
+
+    @Test(timeOut = 20000)
+    public void Scenario2() throws InterruptedException {  
+    	System.out.println("Started senario 2");
+		driver.get(URL);
+		Thread.sleep(3000);
+    	driver.findElement(By.linkText("Checkbox Demo")).click();
+    	WebElement checkbox = driver.findElement(By.id("isAgeSelected"));
+    	checkbox.click();
+    	Assert.assertTrue(checkbox.isSelected(), "Checkbox is not selected!");
+    	checkbox.click();
+    	Assert.assertFalse(checkbox.isSelected(), "Checkbox is still selected!");
+
+    }
+
+    @Test(timeOut = 20000)
+    public void Scenario3() throws InterruptedException{
+		System.out.println("Started senario 3");
+		driver.get(URL);
+		Thread.sleep(3000);
+        driver.findElement(By.linkText("Javascript Alerts")).click();
+        driver.findElement(By.xpath("//button[text()='Click Me']")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals(alert.getText(), "I am an alert box!","Alert message not as expected!");
+        alert.accept();
+
+       
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+          }
+    }
+}
